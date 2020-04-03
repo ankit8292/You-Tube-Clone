e165e375488ba60f27c01f5c39266523df04d2e3
Ankit Gupta <ankitgupta8292@gmail.com>
2020-04-03 17:46:50 +0530

    Update SearchBar.js

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,40 +1,7 @@
 import React, { useState } from 'react'
-//import PropTypes from 'prop-types'
 import {Paper, TextField} from '@material-ui/core'
 
-//  class SearchBar extends Component {
-//     state = {
-//         searchTerm: '',
-//     }
 
-//     handleChange= (event) =>
-//     {
-//         this.setState({
-//             searchTerm: event.target.value
-//         });
-//     }
-
-//     handleSubmit = (event) =>
-//     {
-//         const { searchTerm } = this.state;
-//         const  {onFormSubmit} = this.props;
-
-//         onFormSubmit(searchTerm);
-//         event.preventDefault();
-//     }
-
-//     render() {
-//         return (
-//             <Paper elevation={6} style={{padding: '25px'}}>
-//                 <form onSubmit={this.handleSubmit}>
-//                     <TextField fullWidth label="Search..." onChnage={this.handleChange} />
-
-//                 </form>
-//             </Paper>
-//         )
-//     }
-// }
-// export default SearchBar;
 export default ({ onSubmit }) => {
     const [searchTerm, setSearchTerm] = useState("");
   
